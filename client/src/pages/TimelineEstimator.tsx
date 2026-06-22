deba3b962a26af50e5d86b45cd8efdd39cc77de1
Bailey-Builds <lbailey@sekki.io>
2026-06-22 12:38:41 -0400

    style: use inline style for guaranteed symmetric 8% padding

--- client/src/pages/TimelineEstimator.tsx
+++ client/src/pages/TimelineEstimator.tsx
@@ -7,7 +7,7 @@ export default function TimelineEstimator() {
     <div className="min-h-screen flex flex-col bg-white">
       <Header />
       <main className="flex-1">
-        <div className="px-[5%] sm:px-[7%] lg:px-[8%] py-10">
+        <div className="py-10" style={{ paddingLeft: '8%', paddingRight: '8%' }}>
           <EstimatorWizard />
         </div>
       </main>
