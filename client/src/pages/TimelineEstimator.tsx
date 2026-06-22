b7b0d74e67a36acd7a94c34688e9f16646b99066
Bailey-Builds <lbailey@sekki.io>
2026-06-22 12:34:39 -0400

    style: use percentage-based padding for guaranteed symmetric margins

--- client/src/pages/TimelineEstimator.tsx
+++ client/src/pages/TimelineEstimator.tsx
@@ -7,7 +7,7 @@ export default function TimelineEstimator() {
     <div className="min-h-screen flex flex-col bg-white">
       <Header />
       <main className="flex-1">
-        <div className="px-6 sm:px-10 lg:px-32 xl:px-48 py-10">
+        <div className="px-[5%] sm:px-[7%] lg:px-[8%] py-10">
           <EstimatorWizard />
         </div>
       </main>
